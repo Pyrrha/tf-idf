0a5c212aa1fc5408a869b01cab579a49ab64fb6e
Tristan Dietz <tristan.dietz@laposte.net>
2020-11-19 17:29:15 +0100

    Removing deadcode

--- src/test/java/com/epita/tfidf/service/tokenizer/TestTokenizer.java
+++ src/test/java/com/epita/tfidf/service/tokenizer/TestTokenizer.java
@@ -3,7 +3,6 @@ package com.epita.tfidf.service.tokenizer;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.regex.Pattern;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,8 +10,6 @@ public class TestTokenizer {
 
     @Test
     public void whenPassingUppercaseThenGettingLowerCase() {
-
-        final Pattern textPattern = Pattern.compile("(?=.*[A-Z])");
         final List<Token> listOfTokens = Tokenizer.process("Hello World!");
 
         for (Token token : listOfTokens) {
