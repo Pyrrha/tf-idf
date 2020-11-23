396ed711d2b22eb071a04372075ae8cc1dc46073
YannMOUSSAN <46843092+YannMOUSSAN@users.noreply.github.com>
2020-11-23 16:43:50 +0100

    vector done

--- src/main/java/com/epita/tfidf/service/vector/Vector.java
+++ src/main/java/com/epita/tfidf/service/vector/Vector.java
@@ -1,17 +1,52 @@
 package com.epita.tfidf.service.vector;
 
+import com.epita.tfidf.service.tokenizer.Token;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 public class Vector {
 
-    private ArrayList<String> differentTokens;
-    private ArrayList<Integer> timesTokenUsed;
-    private ArrayList<Integer> percentTokenUsed;
+    private Map<String, VectorValues> vector;
 
-    public Vector(){
-        this.differentTokens = new ArrayList<String>();
-        this.timesTokenUsed = new ArrayList<Integer>();
-        this.percentTokenUsed = new ArrayList<Integer>();
+    public Vector(List<Token> tokens) {
+        this.vector = new HashMap<>();
+    }
+
+    public Map<String, VectorValues> vectorize(final List<String> tokens) {
+        int total = tokens.size();
+        int index = 0;
+
+        for (String token : tokens) {
+            if (this.vector.containsKey(token)) {
+                this.vector.get(token).positions.add(index);
+                this.vector.get(token).frequency += 1/total;
+            }
+            else {
+                this.vector.put(token, new VectorValues(1/total, new ArrayList<>(index)));
+            }
+            ++index;
+        }
+
+        return this.vector;
+    }
+
+    @RequiredArgsConstructor
+    @Data
+    class VectorValues {
+        private int frequency;
+        private List<Integer> positions;
+
+        public VectorValues(int frequency, List<Integer> positions) {
+            this.frequency = frequency;
+            this.positions = positions;
+        }
     }
 
 }
