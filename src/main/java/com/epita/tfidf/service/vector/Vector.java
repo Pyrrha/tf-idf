d63128e825d8bcb2f7dc3d0061686d6045a6659c
YannMOUSSAN <46843092+YannMOUSSAN@users.noreply.github.com>
2020-11-23 15:08:19 +0100

    init vector class

--- src/main/java/com/epita/tfidf/service/vector/Vector.java
+++ src/main/java/com/epita/tfidf/service/vector/Vector.java
@@ -1,4 +1,17 @@
 package com.epita.tfidf.service.vector;
 
+import java.util.ArrayList;
+
 public class Vector {
+
+    private ArrayList<String> differentTokens;
+    private ArrayList<Integer> timesTokenUsed;
+    private ArrayList<Integer> percentTokenUsed;
+
+    public Vector(){
+        this.differentTokens = new ArrayList<String>();
+        this.timesTokenUsed = new ArrayList<Integer>();
+        this.percentTokenUsed = new ArrayList<Integer>();
+    }
+
 }
