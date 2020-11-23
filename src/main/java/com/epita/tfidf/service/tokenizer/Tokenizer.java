e678f506ba8e8ad04bec3aa94a1235245ac7012b
jonathan.paugois <jonathan.paugois@wizway.fr>
2020-11-23 11:37:36 +0100

    Improved words splitting regex

--- src/main/java/com/epita/tfidf/service/tokenizer/Tokenizer.java
+++ src/main/java/com/epita/tfidf/service/tokenizer/Tokenizer.java
@@ -16,12 +16,11 @@ public class Tokenizer {
         final List <Token> listOfTokens = new ArrayList<>();
         String lowerText = cleanText.toLowerCase();
 
-        for (String word : lowerText.split("[ .,;]"))
+        // regex captures single points (.), comas(,), semicolons (;), quotation marks ("),
+        // and spaces with optionally a dash or an apostrophe aside.
+        for (String word : lowerText.split("[.,;\"]|(['-]* ['-]*)"))
             listOfTokens.add(new Token(word));
 
-        // delete stop words
-        // steamming
-        // synonymes
         return listOfTokens;
     }
 }
