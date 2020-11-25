9f24072b48749f44ef16b80543144b1785422e0e
YannMOUSSAN <46843092+YannMOUSSAN@users.noreply.github.com>
2020-11-25 14:05:17 +0100

    synonym class early

--- src/main/java/com/epita/tfidf/service/synonyms/Synonyms.java
+++ src/main/java/com/epita/tfidf/service/synonyms/Synonyms.java
@@ -1,4 +1,33 @@
 package com.epita.tfidf.service.synonyms;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Synonyms {
+
+    private Map<String,String> dicoSyn;
+
+    public Synonyms(){
+
+        dicoSyn = new HashMap();
+        String fileName = "../../../resources/synonyms/en_thesaurus.json";
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+/*
+            for (String line : ):
+                if (nb < 3):
+                    continue;
+                word = steam(line.get("word"));
+                for (String synonym : line.get("data").get("synonyms")):
+                dicoSyn.put(steam(synonym), word);
+
+*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
