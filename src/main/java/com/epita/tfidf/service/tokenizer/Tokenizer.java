370ef75e80cc04943a07e14eb03ab099edaffe7d
jonathan.paugois <jonathan.paugois@wizway.fr>
2020-11-25 11:49:03 +0100

    Refactored Tokenizer to be instanciable

--- src/main/java/com/epita/tfidf/service/tokenizer/Tokenizer.java
+++ src/main/java/com/epita/tfidf/service/tokenizer/Tokenizer.java
@@ -3,26 +3,25 @@ package com.epita.tfidf.service.tokenizer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Tokenizer {
 
-    private Tokenizer() {}
+    private String text;
+
+    private Tokenizer(@NotNull String cleanText) {
+        this.text = cleanText;
+    }
 
     /**
-     * Create an array of tokens from a clean text
-     * @param cleanText String result of a scrapping action
+     * Create an array of tokens from a clean text. Split regex captures single
+     * points (.), comas(,), semicolons (;), quotation marks ("), and spaces
+     * with optionally a dash or an apostrophe aside.
      * @return List a list of tokens that can be use to index
      */
-    public static List<Token> process(@NotNull String cleanText) {
-        final List <Token> listOfTokens = new ArrayList<>();
-        String lowerText = cleanText.toLowerCase();
-
-        // regex captures single points (.), comas(,), semicolons (;), quotation marks ("),
-        // and spaces with optionally a dash or an apostrophe aside.
-        for (String word : lowerText.split("[.,;\"]|(['-]* ['-]*)"))
-            listOfTokens.add(new Token(word));
-
-        return listOfTokens;
+    public List<String> process() {
+        String lowerText = this.text.toLowerCase();
+        return new ArrayList<>(Arrays.asList(lowerText.split("[.,;\"]|(['-]* ['-]*)")));
     }
 }
